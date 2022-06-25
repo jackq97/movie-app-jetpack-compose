c12abf2fcac6a79b62c646d838c6a6124b6f10e4
jaskq97 <jasjassi97@gmail.com>
2022-06-25 16:18:05 +0530

    intial commit

---
+++ app/src/main/java/com/example/jetmovieapp/screens/details/DetailsScreen.kt
@@ -0,0 +1,16 @@
+package com.example.jetmovieapp.screens.home.details
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+// here we gonna add details screen for our app
+// this file is inside a package named details
+
+
+@Composable
+fun DetailsScreen(navController: NavController){
+
+    Text(text = "details screen")
+
+}
